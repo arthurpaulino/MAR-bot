aabfaabf567d845c614167b5a1f7bdc993bf2fe5
Arthur Paulino <arthurleonardo.ap@gmail.com>
2018-01-13 20:11:41 -0300

    more resets after leaving manual mode

--- util/reset_to_auto.s
+++ util/reset_to_auto.s
@@ -4,4 +4,6 @@ reset_to_auto:
     mov [old_target_biomass_x], 0
     mov [old_target_biomass_y], 0
     mov [faced_to_biomass], 0
+    mov [has_migrating_target_cell], 0
+    mov [has_migrating_path], 0
     ret
